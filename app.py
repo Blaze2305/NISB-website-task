bd0f6260944c0c77efb649535941b1458d0c5744
Blaze2305 <prajpran@gmail.com>
2020-06-16 03:20:47 +0530

    home page done

--- app.py
+++ app.py
@@ -23,5 +23,18 @@ def login():
 	if request.method == 'GET':
 		return render_template('login.html')
 
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+	
+@app.route("/gallery")
+def gallery():
+	return 'gallery'
+
+@app.route("/events")
+def events():
+	return 'events'
 if(__name__ == "__main__"):
 	app.run(debug=True,host='0.0.0.0',port=8080)
